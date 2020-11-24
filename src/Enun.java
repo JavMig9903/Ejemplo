8509915aad6e87bbc71716265316eebcaa1eb5e8
Javier M iguel <javiermiguel9903@gmail.com>
2020-11-24 12:11:50 +0100

    Enun

---
+++ src/Enun.java
@@ -0,0 +1,18 @@
+public class Enun {
+    /*
+    Vamos a simular un torneo de Battle Royale, estio warzone
+    En resumen, entran 10 jugadores (hilos) de los que solo puede quedar 1
+
+    Sin embargo inicialmentre, se crean 15(hilos) de los cuales solo los 10 primeros más rapidos entran a la batalla de inmediato.
+    los demas tendran que esperar a que haya hueco.
+
+    nada mas entrar el primer hilo duerme entre 1 y 5 sec
+    el primer hilo que acabe consigue un bonus.
+    los 5 ultimos mueren, dando paso a los hilos que estaban esperando,
+
+    cuando todos los hilos hayan acabado, cada uno de los hilos saca un numero aleatorio que sera la puntuacion y
+    se lo entrega a alguna clase que se encarga de calcular el ganador.
+
+    la puntuacion que hubiera sacado los hilos que tenian bonus vale doble.  gana el hilo que mas puntos haya obtenido
+     */
+}
